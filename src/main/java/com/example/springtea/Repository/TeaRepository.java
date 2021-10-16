4e6b120a35f047e71511b2f939db2d3f527ba543
chirag <chiragdale0@gmail.com>
2021-10-16 17:23:55 +0530

    added some changes

--- src/main/java/com/example/springtea/Repository/TeaRepository.java
+++ src/main/java/com/example/springtea/Repository/TeaRepository.java
@@ -19,19 +19,19 @@ public interface TeaRepository extends JpaRepository<Tea,String> {
 
     Tea getTeaByName(String name);
 
-    @Query(value = "SELECT t.*,\n" +
-            "       GROUP_CONCAT(DISTINCT k.name) AS keyword,\n" +
-            "       GROUP_CONCAT(DISTINCT o.name)  AS origin\n" +
+    @Query(value = "SELECT t.* ,\n" +
+            "       GROUP_CONCAT(DISTINCT k.name) AS keyword ,\n" +
+            "       GROUP_CONCAT(DISTINCT o.name)  AS origin \n" +
             "FROM tea.tea AS t \n" +
             "         INNER JOIN tea.tea_keyword tk on t.id = tk.tea_id \n" +
             "         INNER JOIN tea.keyword k on tk.keyword_id = k.id \n" +
             "         INNER JOIN tea.tea_origin ok on t.id = ok.tea_id \n" +
             "         INNER JOIN tea.origin o on ok.origin_id = o.id \n" +
             "WHERE\n" +
-            "      t.name LIKE %:key% OR\n" +
-            "      t.collection LIKE %:key% OR\n" +
-            "      k.name LIKE %:key% OR\n" +
-            "      o.name LIKE %:key%\n" +
-            "GROUP BY t.id",nativeQuery = true)
+            "      t.name LIKE %:key% OR \n" +
+            "      t.collection LIKE %:key% OR \n" +
+            "      k.name LIKE %:key% OR \n" +
+            "      o.name LIKE %:key% \n" +
+            "GROUP BY t.id ",nativeQuery = true)
     List<Tea> getTeasBySearch(@Param("key") String key);
 }
